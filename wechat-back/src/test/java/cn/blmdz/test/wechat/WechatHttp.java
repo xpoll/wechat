3492771c0be9b0525250cab6b041fa5329f7454c
xpoll <blmdz521@126.com>
2017-12-07 00:37:55 +0800

    wechat

--- wechat-back/src/test/java/cn/blmdz/test/wechat/WechatHttp.java
+++ wechat-back/src/test/java/cn/blmdz/test/wechat/WechatHttp.java
@@ -7,25 +7,78 @@ import com.google.common.collect.Maps;
 
 public class WechatHttp {
 
-	public static String access_token = "ZjtzvVx8s626FJmaqWPImucCTr_7guL0vDe4MjupnIKRz1-bvttAx-vCbGDQFV5llwhiOqc_H50tHrt3inxhMR40gBiNYCJVRoUvqy_AkG19t70aCGEP5aSkdQTC6h3UVDOgAHAIQB";
+	public static String appid = "wx30c11aa9e8d71efc";
+	public static String secret = "2c705bffef34233a31390486c556e1b1";
+	public static String access_token = "GFkh2Iaof9qYtoplL-DWI6wz3n7YYlhOYNrr6k6JJZYJykrt8MNUo1yOYOm5ETQovzF1aM8q5OW0XbgB_ftfwhkTzdffRp6neYcV6RXRU76JAojY9Om7PMcbriFdTIndKNAjAHAYBO";
+	
+	
 	public static String access_token_url = "https://api.weixin.qq.com/cgi-bin/token";
 	public static String ip_url = "https://api.weixin.qq.com/cgi-bin/getcallbackip";
+	public static String access_token_user_get_url = "https://api.weixin.qq.com/sns/oauth2/access_token";
+	public static String access_token_user_refresh_url = "https://api.weixin.qq.com/sns/oauth2/refresh_token";
+	public static String access_token_user_check_url = "https://api.weixin.qq.com/sns/auth";
+	public static String user_info_url = "https://api.weixin.qq.com/sns/userinfo";
+	
 
 	public static void main(String[] args) {
 		Map<String, String> params = Maps.newHashMap();
-		params.put("grant_type", "client_credential");
-		params.put("appid", "wxfc4c9f3eabab8a27");
-		params.put("secret", "6a16f18b3046a1f859e71b2253488783");
-//		get(access_token_url, params);
 		
+		// 获取应用授权access_token
+		params.put("appid", appid);
+		params.put("secret", secret);
+		params.put("grant_type", "client_credential");
+		get(access_token_url, params);
+		
+		// 获取微信服务器IP
 		params.clear();
 		params.put("access_token", access_token);
-		get(ip_url, params);
+//		get(ip_url, params);
+		
+		// 通过code换取网页授权access_token
+		// https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx30c11aa9e8d71efc&redirect_uri=http://blmdz.cn/third/test&response_type=code&scope=snsapi_base&state=123#wechat_redirect
+		// https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx30c11aa9e8d71efc&redirect_uri=http://blmdz.cn/third/test&response_type=code&scope=snsapi_userinfo&state=123#wechat_redirect
+		params.clear();
+		params.put("appid", appid);
+		params.put("secret", secret);
+		params.put("grant_type", "authorization_code");
+		params.put("code", "081RGH3L0eCoY62PuM6L00bB3L0RGH3O");
+//		get(access_token_user_get_url, params);
+		
+		// 刷新access_token（如果需要）
+		params.clear();
+		params.put("appid", appid);
+		params.put("grant_type", "refresh_token");
+		params.put("refresh_token", "4_ypR-F-SuEaDeMpy2eWk3aLMzNj3F99HStzehDkI6GlAzaK5XHVbD5X74QPz-oUmnL94XlcrHYFGzm9C4gTsqKpJFIr9efNxQpHTAt0ZOeGQ");
+//		get(access_token_user_refresh_url, params);
+		
+		// 拉取用户信息(需scope为 snsapi_userinfo)
+		params.clear();
+		params.put("access_token", "4_OsAuNN3e60k3QPIA36RLZtWC-Cp_0TqKhxgR2T6sN7UWdltZSwh53UPQsbya2w_DPDjBsKc0VzWmC2dtC-y6bOW5Z0wR9K73LLdkm33plc8");
+		params.put("lang", "zh_CN");
+		params.put("openid", "oE0eJ0bp0ZxkXEnzClPEJUDclljg");
+//		get(user_info_url, params);
+		
+		// 检验授权凭证（access_token）是否有效
+		params.clear();
+		params.put("access_token", "4_OsAuNN3e60k3QPIA36RLZtWC-Cp_0TqKhxgR2T6sN7UWdltZSwh53UPQsbya2w_DPDjBsKc0VzWmC2dtC-y6bOW5Z0wR9K73LLdkm33plc8");
+		params.put("openid", "oE0eJ0bp0ZxkXEnzClPEJUDclljg");
+//		get(access_token_user_check_url, params);
 	}
 
 	public static void get(String url, Map<String, String> params) {
 		HttpRequest request = HttpRequest.get(url, params, false);
 		System.out.println(request.ok());
 		System.out.println(request.body());
+	}
+	/**
+	 * post
+	 */
+	public static void post(String url, String msg) {
+		HttpRequest request = HttpRequest.post(url);
+		request.header(HttpRequest.HEADER_CONTENT_TYPE, HttpRequest.CONTENT_TYPE_JSON);
+		request.send(msg);
+		System.out.println(request.ok());
+		System.out.println(request.body());
+		
 	}
 }
